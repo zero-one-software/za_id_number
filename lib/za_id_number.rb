f85096c41e687da48f82ac61cd7114fdb9eec0c1
Gabriel Fortuna <gabriel@zero-one.io>
2017-12-01 16:47:56 +0200

    refactor: make validation less naive

--- lib/za_id_number.rb
+++ lib/za_id_number.rb
@@ -2,15 +2,29 @@ require 'za_id_number/version'
 require 'luhn'
 
 class ZAIDNumber
-  VERSION = "0.1.0"
+  REQUIRED_ID_LENGTH = 13
 
   attr_reader :id_number
 
   def initialize(id_num)
-    @id_number = id_num
+    @id_number = id_num.to_s
   end
 
   def valid?
+    has_valid_length? &&
+    has_only_digits?  &&
+    has_valid_checksum?
+  end
+
+  def has_valid_checksum?
     Luhn.valid? @id_number
+  end
+
+  def has_valid_length?
+    @id_number.length == REQUIRED_ID_LENGTH
+  end
+
+  def has_only_digits?
+    @id_number.to_s.gsub(/\D*/, '') == @id_number.to_s
   end
 end
--- spec/za_id_number_spec.rb
+++ spec/za_id_number_spec.rb
@@ -2,20 +2,47 @@ require "spec_helper"
 require 'pry'
 
 describe ZAIDNumber do
-  let(:valid_za_id)   { 7501151234086 }
-  let(:invalid_za_id) { 7501151234087 }
+  let(:valid_za_id)   { 7501151234085.to_s }
+  let(:invalid_za_id) { 7501151234087.to_s }
+
+  subject { described_class.new(valid_za_id) }
 
   it "has a version number" do
-    expect(ZAIDNumber::VERSION).not_to be nil
+    expect(ZAIDNumber::Version::VERSION).not_to be nil
   end
 
   context "validation checks" do
-    it "should detect valid ZA ID numbers" do
-      expect(described_class.new(valid_za_id)).to be_valid
+    it "should only allow 13 character long id numbers" do
+      expect(subject).to have_valid_length
+
+      expect(described_class.new("12345678901234")).to_not have_valid_length
+      expect(described_class.new("123456789012")).to_not   have_valid_length
     end
 
-    it "should detect invalid ZA ID numbers" do
-      expect(described_class.new(invalid_za_id)).to_not be_valid
+    it "should only allow digits" do
+      expect(subject).to                                     have_only_digits
+      expect(described_class.new("a#{valid_za_id}a")).to_not have_only_digits
+    end
+
+    it "should detect valid ZA ID number checksums" do
+      expect(subject).to have_valid_checksum
+    end
+
+    it "should detect valid number checksums starting with 0" do
+      num = "0001011234083"
+      expect(described_class.new(num)).to have_valid_checksum
+    end
+
+    it "should detect invalid ZA ID number checksums" do
+      expect(described_class.new(invalid_za_id)).to_not have_valid_checksum
+    end
+
+    it "should ensure ID numbers are only 13 digit numbers with valid checksums" do
+      expect(subject).to be_valid
+
+      expect(described_class.new("123456789015")).to_not  be_valid # all digits and has correct checksum, but too short
+      expect(described_class.new("1234567A89012")).to_not be_valid # not all digits, but correct length
+      expect(described_class.new(invalid_za_id)).to_not   be_valid # all digits, and correct length, but bad checksum
     end
   end
 end
